b05119c979feb91d37ccda6f394c544964581801
diak <diak@mesterlevel.lo>
2026-02-26 12:53:27 +0100

    Added ratings + bookRoutes

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const userRoutes = require('./routes/userRoutes')
 
 const app = express()
 app.use(cors({
-    origin: ['http://127.0.0.1:5173&#39;,']
+    origin: ['http://127.0.0.1:5173'],
+    credentials: 'include'
 }))
 
 app.use(express.json())
