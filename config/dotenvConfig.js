e425675d66f0afc63b40eafb1bd461988f407e1b
jhin <elekmark01@gmail.com>
2026-04-22 13:11:42 +0200

    cors

--- config/dotenvConfig.js
+++ config/dotenvConfig.js
@@ -6,6 +6,7 @@ const config = {
     HOST: process.env.HOST,
     PORT: process.env.PORT,
     DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
     DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD,
     DB_NAME: process.env.DB_NAME,
